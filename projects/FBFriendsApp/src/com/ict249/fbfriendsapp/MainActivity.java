c92190142bf1275cd073be0d63df99534532b401
charlieluna <charlielouna@gmail.com>
2013-06-25 00:08:58 +0800

    initial commit for facebook plugin integrated application

---
+++ projects/FBFriendsApp/src/com/ict249/fbfriendsapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.ict249.fbfriendsapp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import org.apache.cordova.DroidGap;
+
+import com.ict249.facebookapp.R;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
